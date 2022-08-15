d425c7133006559aaa3b8574eeddfd27d0d79156
Amir <103879813+Amir-pn@users.noreply.github.com>
2022-08-15 21:21:26 +0430

    Update Gaussian-dis.py

--- Gaussian-dis.py
+++ Gaussian-dis.py
@@ -3,7 +3,7 @@ from skimage import io
 import matplotlib.pyplot as plt
 from skimage.filters import gaussian
 
-img3= io.imread("C:/Users/dell/Desktop/Project/images/image3.jpg",as_gray=True)
+img3= io.imread(".../image.jpg",as_gray=True)
 gaus1= gaussian(img3,sigma= 1.3)
 plt.figure('gaussian_1')
 plt.imshow(gaus1,cmap='gray')
@@ -13,7 +13,7 @@ plt.imshow(gaus2,cmap='gray')
 gaus3= gaussian(img3,sigma= 0.08)
 plt.figure('gaussian_0.09')
 plt.imshow(gaus3,cmap='gray')
-plt.imsave("C:/Users/dell/Desktop/Project/images/Results/Image(3).Gaussian_distribution/Image_1.jpg",gaus1,cmap='gray')
-plt.imsave("C:/Users/dell/Desktop/Project/images/Results/Image(3).Gaussian_distribution/Image_2.jpg",gaus2,cmap='gray')
-plt.imsave("C:/Users/dell/Desktop/Project/images/Results/Image(3).Gaussian_distribution/Image_3.jpg",gaus3,cmap='gray')
+plt.imsave(".../Image_1.jpg",gaus1,cmap='gray')
+plt.imsave(".../Image_2.jpg",gaus2,cmap='gray')
+plt.imsave(".../Image_3.jpg",gaus3,cmap='gray')
 plt.show()
